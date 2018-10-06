dce76549a8d180019784903f0545be1651d276fb
Zaira Tomayeva <zaira.tomayeva@procore.com>
2018-10-06 02:27:47 -0700

    implemented the Sieve of Eratosthenes
    stored in 2 arrays: one for the numbers and the other to indicate if
    they’re prime or not

--- app/src/main/java/zairat/android/primetime/MainActivity.kt
+++ app/src/main/java/zairat/android/primetime/MainActivity.kt
@@ -2,11 +2,26 @@ package zairat.android.primetime
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val max = 25
+        val numbers = Array(max-1){i -> i+2}
+        val isPrime  = BooleanArray(max-1) {true}
+
+        for (i in numbers.indices){
+            if (isPrime[i]){
+                var temp = (i+2)*(i+2)
+                while (temp <= max){
+                    isPrime[temp-2] = false
+                    temp += (i+2)
+                }
+            }
+        }
     }
 }
